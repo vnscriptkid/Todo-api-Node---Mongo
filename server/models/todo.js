d1bbf1502d660696ed3517b7322c4d69c96c487b
vnscriptkid <vnscriptkid@gmail.com>
2017-10-15 19:43:07 +0700

    Add POST /todos route

---
+++ server/models/todo.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+
+var Todo = mongoose.model('Todo', {
+	text: {
+		type: String,
+		required: true,
+		minlength: 3,
+		trim: true
+	},
+	completed: {
+		type: Boolean,
+		default: false,
+		required: true
+	},
+	completedAt: {
+		type: Number,
+		default: null
+	}
+});
+
+module.exports = {Todo};
