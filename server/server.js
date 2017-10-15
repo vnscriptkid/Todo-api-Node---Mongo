4017ef5f0bfae2a1dc8da0d2045dd4b876c704e3
vnscriptkid <vnscriptkid@gmail.com>
2017-10-15 23:58:18 +0700

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/todo');
+
 
 var app = express();
 
@@ -28,6 +30,22 @@ app.get('/todos', (req, res) => {
 		res.status(400).send(err);
 	});
 });
+
+app.get('/todos/:id', (req, res) => {	
+	var id = req.params.id;
+	if(!ObjectID.isValid(id)){
+		return res.status(404).send("id provided not valid");
+	}
+	Todo.findById(req.params.id).then((todo) => {
+		if(!todo){
+			return res.status(404).send({});
+		}
+		res.status(200).send({todo});
+	})
+	.catch((e) => {
+		res.status(400).send(e.message);
+	});
+})
 
 app.listen(30001, () => {
 	console.log('Started on port 30001');
