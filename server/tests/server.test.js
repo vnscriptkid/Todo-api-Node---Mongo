9428fd4416357359bb80db73f68e8102e4c3723b
vnscriptkid <vnscriptkid@gmail.com>
2017-10-15 21:52:31 +0700

    Adding tests for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,8 +4,20 @@ const request = require('supertest');
 const {app} = require('../server');
 const {Todo} = require('../models/todo');
 
+const todos = [
+	{
+		text: 'First test todo'
+	},
+	{
+		text: 'Second test todo'
+	}
+]
+
 beforeEach((done) => {
-	Todo.remove({}).then(() => done());	
+	// Todo.remove({}).then(() => done());	
+	Todo.remove({}).then(() => {
+		return Todo.insertMany(todos);
+	}).then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -23,7 +35,7 @@ describe('POST /todos', () => {
 				if(err){
 					return done(err);					
 				}
-				Todo.find().then((todos) => {
+				Todo.find({text}).then((todos) => {
 					expect(todos.length).toBe(1);
 					expect(todos[0].text).toBe(text);
 					done();
@@ -42,9 +54,21 @@ describe('POST /todos', () => {
 						return done(err);
 					}
 					Todo.find().then((todos) => {
-						expect(todos.length).toBe(0);
+						expect(todos.length).toBe(2);
 						done();						
 					}).catch((e) => done(e));
 				});
 	});
 });
+
+describe('GET /todos', () => {
+	it('should get all todos', (done) => {
+		request(app)
+			.get('/todos')
+			.expect(200)
+			.expect((res) => {
+				expect(res.body.docs.length).toBe(2);
+			})
+			.end(done);
+	});
+})
